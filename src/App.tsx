42251e84a6bf7237ebc6d290040b3a080bb11a03
chubi-x <chubixax@protonmail.com>
2022-10-15 13:52:13 +0100

    scaffold app

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./index.css";
+import logo from "./img.png";
+const App = () => {
+  return (
+    <div className="classnans">
+      <h1>hello world</h1>
+      <img src={logo} alt="" />
+    </div>
+  );
+};
+export default App;
